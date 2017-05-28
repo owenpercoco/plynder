3a00aa1757f50a99ef9b3b4710c31a8fe214b4e9
owenpercoco <owenpercoco@gmail.com>
2017-05-28 17:29:57 -0400

    optimization

--- plynder.py
+++ plynder.py
@@ -6,13 +6,13 @@ from FB import *
 
 def like_everybody(sess):
         girls = session.nearby_users()
-        chance_dislike = random.randrange(0, 50)
+        chance_dislike = random.randrange(0, 12)
 	for girl in girls:
-        	d = random.randrange(0, 50)
+        	d = random.randrange(0, 12)
                 if d == chance_dislike:
 			girl.dislike
                 else:
-			time.sleep(random.randrange(0, 1))
+			time.sleep(random.randrange(0, 2))
                         print girl.name
                         print girl.photos[0]
                         print girl.instagram_username
@@ -20,9 +20,13 @@ def like_everybody(sess):
 
 session = pynder.Session(facebook_id = f_id, facebook_token = f_token)
 session.matches() # get users you have already been matched with
-print 'you have ' + str(len([i for i in session.matches()])) +  ' matches'
-for babes in session.matches():
-        print babes
+count = 0
+babes = ""
+for i in session.matches():
+	count += 1
+	babes = babes + str(i) + " \n"
+print 'you have ' + str(count) +  ' matches'
+print babes
 
 print session.likes_remaining
 if session.likes_remaining > 0:
